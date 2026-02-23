e43269f33d32bc98d37df694feeaaec5f843d771
Victor Segura <vseguranes@gmail.com>
2026-02-23 16:57:53 +0100

    Declaración de 2 socios, 2 clases y la muestra por pantalla de la información de cada uno

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        Socio socio1 = new Socio(1,"Antony");
+        Socio socio2 = new Socio(2,"Mortadelo");
+
+        Sala sala1 = new Sala(1,"Aquí es donde los astronautas van a la luna");
+        Sala sala2 = new Sala(2,"Sala solo accesible para ganadores de un hackaton");
+
+        System.out.println(socio1.toString());
+        System.out.println(socio2.toString());
+
+        System.out.println(sala1.toString());
+        System.out.println(sala2.toString());
     }
 }
