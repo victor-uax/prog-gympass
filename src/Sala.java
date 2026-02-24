6e79408e1f0082b3898344c669f3cdc9e9ee3ac6
Victor Segura <vseguranes@gmail.com>
2026-02-24 08:54:05 +0100

    Realización de los comentarios del código

--- src/Sala.java
+++ src/Sala.java
@@ -3,20 +3,35 @@ public class Sala {
     private int codigoSala;
     private String descripcion;
 
+    /**
+     * Constructor de la clase Sala donde se definen los valores de int codigoSala
+     * y String descripcion
+     * @param codigoSala
+     * @param descripcion
+     */
     public Sala(int codigoSala, String descripcion) {
         this.codigoSala = codigoSala;
         this.descripcion = descripcion;
     }
-
+    /**
+     * Metodo el cual devuelve el código de la sala
+     * @return codigoSala
+     */
     public int getCodigoSala() {
         return codigoSala;
     }
-
+    /**
+     * Metodo el cual devuelve en forma de String la descripción de la sala
+     * @return descripcion
+     */
     public String getDescripcion() {
         return descripcion;
     }
-
+    /**
+     * Metodo para hacer un String juntando la descripcion con el codigo de la sala
+     * @return String
+     */
     public String toString() {
-        return "Sala{" + "codigoSala=" + codigoSala + ", descripcion=" + descripcion + '}';
+        return "Sala{ " + "codigoSala=" + codigoSala + ", descripcion=" + descripcion + '}';
     }
 }
--- src/Socio.java
+++ src/Socio.java
@@ -3,19 +3,36 @@ public class Socio {
     private final int numSocio;
     private final String nombre;
 
+    /**
+     * Constructor de la clase Socio donde se definen los valores de numeroSocio
+     * y String nombre
+     * @param numeroSocio
+     * @param nombre
+     */
     public Socio(int numeroSocio, String nombre) {
         this.numSocio = numeroSocio;
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo el cual devuelve el número del socio
+     * @return numSocio
+     */
     public int getNumSocio() {
         return numSocio;
     }
-
+    /**
+     * Metodo el cual devuelve en forma de String el nombre del socio
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo para hacer un
+     * @return
+     */
     public String toString() {
         return "Socio{ numSocio = " + numSocio + ", nombre = " + nombre + "}";
     }
