e5934b90fb0d73889f71c0d8b219e1e887613d67
Victor Segura <vseguranes@gmail.com>
2026-02-25 10:17:04 +0100

    feat(Gimnasio): buscarSocioPorNumero para poder implementarlo en expulsarSocio

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -5,6 +5,7 @@ public class Gimnasio {
     private Socio[] listaSocios;
     private Sala[] listaSalas;
     private int responsableId;
+    private int contadorSocios;
 
     private final int CANT_SOCIOS;
     private final int CANT_SALAS;
@@ -37,12 +38,13 @@ public class Gimnasio {
 
         posicionLibre = devolverPosicionLibreSocios();
 
-        if(posicionLibre != -1 && !comprobarDuplicidadSocio(s)){
+        if(posicionLibre != -1 && comprobarDuplicidadSocio(s) != -1){
             listaSocios[posicionLibre] = s;
             socioRegistrado = true;
         }
         return socioRegistrado;
     }
++
 
     /**
      * Este metodo recorre el array de listaSocios buscando el primer hueco libre y si lo encuentra devuelve la posición
@@ -62,22 +64,62 @@ public class Gimnasio {
         return posicionVacia;
     }
 
+    /*
+    private int buscarSocio(int numeroSocio){
+         boolean seguirBuscando = true;
+         int posicionSocioEncontrado = -1;
+         //int numeroDeSocio;
+
+        for (int i = 0; i < CANT_SOCIOS && seguirBuscando; i++) {
+            if(numeroSocio == listaSocios[i].getNumSocio()){
+                posicionSocioEncontrado = i;
+                seguirBuscando = false;
+            }
+        }
+        return posicionSocioEncontrado;
+    }
+    */
+
     /**
      * Comprueba que el código del Socio s no esté duplicado en el array listaSocios
-     * @param s
+     * @param
      * @return true si está duplicado, false si no está duplicado
      */
-    private boolean comprobarDuplicidadSocio(Socio s){
+    private int comprobarDuplicidadSocio(Socio s){
         boolean seguirBuscando = true;
-        boolean socioDuplicado = true;
+        int posicionSocioDuplicado = -1;
 
         for (int i = 0; i < CANT_SOCIOS && seguirBuscando; i++) {
             if( !(listaSocios[i].getNumSocio() == (s.getNumSocio()))){
                 seguirBuscando = false;
-                socioDuplicado = false;
+                posicionSocioDuplicado = i;
             }
         }
-        return socioDuplicado;
+        return  posicionSocioDuplicado;
     }
 
+    private int buscarSocioPorNumero(int numeroSocio){
+        boolean seguirBuscando = true;
+        int posicionSocioDuplicado = -1;
+
+        for (int i = 0; i < CANT_SOCIOS && seguirBuscando; i++) {
+            if( !(listaSocios[i].getNumSocio() == numeroSocio){
+                seguirBuscando = false;
+                posicionSocioDuplicado = i;
+            }
+        }
+        return  posicionSocioDuplicado;
+    }
+
+
+    private Socio expulsarSocio(int numeroSocio){
+        Socio socioExpulsado = null;
+
+        for (int i; i < CANT_SOCIOS; i++) {
+            if (buscarSocioPorNumero(numeroSocio) == -1) {
+
+            }
+        }
+
+    }
 }
